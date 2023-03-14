f64268df4e731d317d701939efc2d31b08c75b26
Surakshitha Suresh <suraksha.s09@gmail.com>
2023-03-14 00:11:01 -0700

    **add resoruces(string, dimensions, theme, color, backgroundimg), style to buttons

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -8,14 +8,6 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.ceil
 
-/**
- * Number of Slices per pizza
- */
-const val SLICES_PER_PIZZA = 8
-
-/**
- * Main class
- */
 class MainActivity : AppCompatActivity() {
 
     /**
@@ -31,14 +23,16 @@ class MainActivity : AppCompatActivity() {
     /**
      * The hungry ratio radio button - light, medium and ravenous
      */
-    private lateinit var degreeOfHunger : RadioGroup
+    private lateinit var hungryRatio: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         numberOfPeople = findViewById(R.id.number_of_people_edit_text)
         totalPizzas = findViewById(R.id.total_pizzas_text_view)
-        degreeOfHunger = findViewById(R.id.hungry_ratio_radio_group)
+        val totalText = getString(R.string.total_pizzas, 0)
+        totalPizzas.text = totalText
+        hungryRatio = findViewById(R.id.hungry_ratio_radio_group)
     }
 
     fun calculateClick(view: View) {
@@ -48,14 +42,17 @@ class MainActivity : AppCompatActivity() {
         val headCount = numberOfPeople.text.toString().toInt()
 
         // Calculate number of pizza slices needed per head depending on how hungry the gang is
-        val slicesPerPerson = when (degreeOfHunger.checkedRadioButtonId) {
-            R.id.light_radio_button -> 2
-            R.id.medium_radio_button -> 3
-            else -> 4
+        val hungerLevel = when (hungryRatio.checkedRadioButtonId) {
+            R.id.light_radio_button -> PizzaCalculator.HungerLevel.LIGHT
+            R.id.medium_radio_button -> PizzaCalculator.HungerLevel.MEDIUM
+            else -> PizzaCalculator.HungerLevel.RAVENOUS
         }
 
+        val calc = PizzaCalculator(headCount, hungerLevel)
+        val totalPizzas = calc.totalPizzas
+
         // Final calculation for the total pizzas to be bought
-        val totalPizzas = ceil(headCount * slicesPerPerson / SLICES_PER_PIZZA.toDouble()).toInt()
-        this.totalPizzas.text = "Total pizzas: $totalPizzas"
+        val totalText = getString(R.string.total_pizzas, totalPizzas)
+        this.totalPizzas.text = totalText
     }
 }
